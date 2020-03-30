61bf98c565269e67399c7fddcd1a9e21a14e4eaa
Guy Baron <guyb2000@gmail.com>
2020-03-31 00:23:58 +0300

    added not found page design

--- ollopa/src/app/app.module.ts
+++ ollopa/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
-import { HomeComponent } from './features/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
--- ollopa/src/app/features/page-not-found/page-not-found.module.ts
+++ ollopa/src/app/features/page-not-found/page-not-found.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [PageNotFoundComponent],
   imports: [
     CommonModule,
-    PageNotFoundRoutingModule
+    PageNotFoundRoutingModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class PageNotFoundModule { }
