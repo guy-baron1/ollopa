d723d1a2fd52e1dda0b6aab70b9e7a880b9100d5
Guy Baron <guyb2000@gmail.com>
2020-03-30 20:05:51 +0300

    added base angular material packages

--- ollopa/src/app/app.module.ts
+++ ollopa/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { HomeComponent } from './features/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { HomeComponent } from './features/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
