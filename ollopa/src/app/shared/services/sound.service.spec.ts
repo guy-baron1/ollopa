8f34151227f5f46873ea8142e18ee35fd0bff32f
Guy Baron <guyb2000@gmail.com>
2020-04-03 20:09:19 +0300

    added testing data to sound service

--- ollopa/src/app/shared/services/sound.service.spec.ts
+++ ollopa/src/app/shared/services/sound.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LoginService } from './login.service';
+import { SoundService } from './sound.service';
 
-describe('LoginService', () => {
-  let service: LoginService;
+describe('SoundService', () => {
+  let service: SoundService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(LoginService);
+    service = TestBed.inject(SoundService);
   });
 
   it('should be created', () => {
--- ollopa/src/app/shared/services/sound.service.ts
+++ ollopa/src/app/shared/services/sound.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Sound } from 'src/app/shared/models/sound';
+import { delay } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,12 @@ export class SoundService {
   constructor() { }
 
   getParameterSounds(parameter: string): Observable<Sound[]> {
-    // this should request data from server
-    return new Observable<Sound[]>();
+    // THIS IS JUST FOR TESTING, WILL BE REPLACED WITH REAL API CALL
+    const sounds: Sound[] = [
+      { path: 'asdasd' },
+      { path: 'qweqwe' },
+      { path: 'ertert' }
+    ];
+    return of(sounds).pipe(delay(1000));
   }
 }
