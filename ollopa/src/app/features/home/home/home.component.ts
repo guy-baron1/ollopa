159393f4b67e439607751ed2f95751f18d7668d0
Guy Baron <guyb2000@gmail.com>
2020-05-16 15:53:53 +0300

    added section display to home component

--- ollopa/src/app/features/home/home/home.component.ts
+++ ollopa/src/app/features/home/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { GlobalStoreState, GlobalStoreSelectors, GlobalStoreActions } from 'src/app/root-store/global-store';
+import { Observable } from 'rxjs';
 
 interface ButtonToRoute {
   name: string;
@@ -12,17 +15,21 @@ interface ButtonToRoute {
 })
 
 export class HomeComponent implements OnInit {
+  constructor(private store: Store<GlobalStoreState.State>) { }
+
   public searchValue: string;
+  public section$: Observable<string> = this.store.select(
+    GlobalStoreSelectors.selectGlobalSection
+  );
   public buttonNamesToRoutes: ButtonToRoute[] = [
     {name: 'משימות', route: 'temp'},
     {name: 'תמציות', route: 'temp'},
     {name: 'גרף הידע', route: 'temp'},
-    {name: 'לא זוכר', route: 'temp'},
+    {name: 'מנועים', route: 'temp'},
   ];
-  constructor() { }
 
   ngOnInit(): void {
-    // todo: get section from store?
+    this.store.dispatch(new GlobalStoreActions.GetSectionRequestAction());
   }
 
   public search() {
